926bd114bf5f54636bd136a32509bce7788b96f2
Lee Pavelich <leepavelich@gmail.com>
2022-01-18 05:26:49 +0000

    Refactored out setupInput code to separted file

--- play.js
+++ play.js
@@ -1,26 +1,7 @@
 const connect = require('./client');
-
-// setup interface to handle user input from stdin
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    console.log('User disconnected from the server');
-    process.exit();
-  }
-};
+const setupInput = require('./input')
 
 console.log("Connecting ...");
-setupInput();
 connect();
+
+setupInput();

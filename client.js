6f154a53d9bcf706bac1d7980055d8824a02492e
Lee Pavelich <leepavelich@gmail.com>
2022-01-18 05:21:20 +0000

    Linted

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "localhost",
     port: 50541
@@ -11,17 +11,17 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log('Successfully connected to game server')
-    conn.write('Name: LEE')
+    console.log('Successfully connected to game server');
+    conn.write('Name: LEE');
 
     // TEST CODE
     // setInterval(() => conn.write('Move: up'), 200)
-  })
+  });
 
   conn.on("data", data => {
     console.log(data.toString());
     conn.end();
-  })
+  });
 
   return conn;
 };
--- play.js
+++ play.js
@@ -1,26 +1,26 @@
-const connect = require('./client')
+const connect = require('./client');
 
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  stdin.on("data", handleUserInput)
+  stdin.on("data", handleUserInput);
 
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
-    console.log('User disconnected from the server')
+    console.log('User disconnected from the server');
     process.exit();
   }
-}
+};
 
 console.log("Connecting ...");
-setupInput()
+setupInput();
 connect();
